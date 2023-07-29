a77b5e1e2b21a41448ac3388b9bb2d08fa725b14
b3nji87 <77153014+b3nji87@users.noreply.github.com>
2023-07-29 22:00:00 +0200

    Create generate_samples.sh

---
+++ generate_samples.sh
@@ -0,0 +1,5 @@
+python sonnet_gen.py -m output/quatrain -n 500 -a 1.0 -b 1.0 >> ds500t1.txt
+python sonnet_gen.py -m output/quatrain -n 500 -a 0.7 -b 0.7 >> ds500t07.txt
+
+python sonnet_gen.py -m output/quatrain_de -n 500 -a 1.0 -b 1.0 >> ds500t1_de.txt
+python sonnet_gen.py -m output/quatrain_de -n 500 -a 0.7 -b 0.7 >> ds500t07_de.txt
